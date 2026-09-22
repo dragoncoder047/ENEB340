23e79a87b18ea94699fab90c3675497cd2156490
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2026-09-22 16:36:56 -0400

    speed test

--- lab4/lab4-1.c
+++ lab4/lab4-1.c
@@ -3,7 +3,6 @@
 #ifndef KW
 #define KW register
 #endif
-_Static_assert(sizeof(long double) != sizeof(double));
 int main(int argc, char** argv) {
     KW unsigned long long x = strtoull(argv[1], NULL, 10);
     KW long double f = 1.;
